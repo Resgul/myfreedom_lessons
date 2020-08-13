9562fba660e5b52625375e2d002a097642c65a6a
Resgul <60659541+Resgul@users.noreply.github.com>
2020-08-13 17:22:14 +0300

    добавил дедлайн

--- 7_homework/script.js
+++ 7_homework/script.js
@@ -3,11 +3,62 @@ let ol = document.querySelector('.ol-1');
 let inputMain = document.querySelector('.input-1');
 let messageType = document.querySelector('select');
 let ulDone = document.querySelector('.done-list');
+let daySelect = document.querySelector('.day');
+let monthSelect = document.querySelector('.month');
+
+function dataSet() {
+  let month = [{mon:'январь', mona:'января'},
+              {mon:'февраль', mona:'февраля'},
+              {mon:'март', mona:'марта'},
+              {mon:'апрель', mona:'апреля'},
+              {mon:'май', mona:'мая'},
+              {mon:'июнь', mona:'июня'},
+              {mon:'июль', mona:'июля'},
+              {mon:'август', mona:'августа'},
+              {mon:'сентябрь', mona:'сентября'},
+              {mon:'октябрь', mona:'октября'},
+              {mon:'ноябрь', mona:'ноября'},
+              {mon:'декабрь', mona:'декабря'}];
+  let month31 = ['января', 'марта', 'мая', 'июля', 'августа', 'октября', 'декабря'];
+  let month30 = ['апреля', 'июня', 'сентября', 'ноября'];
+  
+  //Заполняю циклом for селект с месяцами
+  for (let i = 0; i < month.length; i++) {
+    let monthOption = document.createElement('option');
+    monthOption.textContent = month[i].mon;
+    monthOption.setAttribute('value', `${month[i].mona}`);
+    monthSelect.appendChild(monthOption);
+  }
+
+  //По клику на селект с месяцами генерируется количество дней для селекта дней
+  monthSelect.addEventListener('click', () => {
+    daySelect.textContent = '';
+
+    //Если месяц входит в один из массивов с месяцами, выбирается кол-во дней для следующего цикла for
+    if (month31.includes(monthSelect.value)) {
+      var days = 31;
+    } else {
+        if (month30.includes(monthSelect.value)) {
+          var days = 30;
+        } else {
+            var days = 28;
+          }
+      }
+    
+    //Заполняю селект с количеством дней, он зависит от выбранного месяца
+    for (let i = 1; i <= days; i++) {
+      let dayOption = document.createElement('option');
+      dayOption.textContent = i;
+      dayOption.setAttribute('value', `${i}`);
+      daySelect.appendChild(dayOption);
+    }
+  })
+}
+dataSet();
+
 formTask.addEventListener('submit', event => {
   event.preventDefault();
-  
   let newLi = document.createElement('li');
-  
   //В спан я буду записывать текст из инпута
   let newSpan = document.createElement('span');
   newSpan.textContent = document.querySelector('.input-1').value;
@@ -25,9 +76,11 @@ formTask.addEventListener('submit', event => {
   redButton.addEventListener('click', () => {
     let redInput = document.createElement('input');
     newLi.append(redInput);
-    redInput.addEventListener('mouseout', () => {
-      newSpan.textContent = redInput.value;
-      redInput.remove();
+    redInput.addEventListener('keyup', (event) => {
+      if (event.keyCode === 13) {  
+        newSpan.textContent = redInput.value;
+        redInput.remove();
+      }
     })
   })
 
@@ -45,12 +98,16 @@ formTask.addEventListener('submit', event => {
       if (newSpan.textContent !='') {
         let doneLi = newLi;
         doneLi.textContent = newSpan.textContent; 
-        ulDone.append(doneLi)
+        ulDone.append(doneLi);
         doneLi.classList.add('checked');
       }
     })
 
+    let dataP = document.createElement('p');
+    dataP.textContent = `${daySelect.value}-го ${monthSelect.value}`;
+    //Вывод в <li>: текста в <span>,даты через фунецию, которая возвращает строку, удаление, редактирование
     newLi.append(newSpan);
+    newLi.append(dataP);
     newLi.appendChild(delButton);
     newLi.appendChild(redButton);
 
