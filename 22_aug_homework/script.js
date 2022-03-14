6be1a667a0ddc142fb32fa793e93fd1a630edb8b
ResGul <reshad-xxi@yandex.ru>
2022-03-14 20:07:59 +0300

    22 - тест в DOM, добавление среды testEnvironment: jsdom

--- 22_aug_homework/script.js
+++ 22_aug_homework/script.js
@@ -1,3 +1,8 @@
 export function sum(array) {
   return array.reduce((prev, curr) => prev += Number(curr), 0);
 }
+
+export function sumLi() {
+  return [...document.querySelectorAll('li')].map(li => li.innerHTML).reduce((prev, curr) => prev += Number(curr), 0);
+}
+console.log(sumLi());
--- 22_aug_homework/script.test.js
+++ 22_aug_homework/script.test.js
@@ -1,4 +1,4 @@
-import {sum} from './script';
+import {sum, sumLi} from './script';
 
 test('sum(arr): sums up numbers in array', () => {
   const result = sum([1, 2, 3, '6']);
@@ -8,4 +8,17 @@ test('sum(arr): sums up numbers in array', () => {
 test('sum(arr): sums up numbers in array', () => {
   const result = sum([1, 2, 3]);
   expect(result).toBe(6);
+})
+
+test('sumLi(arr): sums up numbers in TAG <li>', () => {
+  document.body.innerHTML = `  
+  <ul>
+    <li>2</li>
+    <li>4</li>
+    <li>6</li>
+    <li>8</li>
+  </ul>`
+  
+  const result = sumLi();
+  expect(result).toBe(20);
 })
